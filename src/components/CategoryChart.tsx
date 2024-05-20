c0fbbfa52cdfa4f30b01276bb5d436270f3a12dc
rikooo <riko.05.mac@gmail.com>
2024-05-20 17:07:11 +0900

    円グラフのカラーを変更

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import { Box, CircularProgress, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import { Box, CircularProgress, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography, useTheme } from '@mui/material';
 import { ExpenseCategory, IncomeCategory, Transaction, TransactionType } from '../types';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -12,6 +12,7 @@ interface CategoryChartProps {
 }
 
 const CategoryChart = ({ monthlyTransactions, isLoading }:CategoryChartProps ) => {
+  const theme = useTheme();
   const [selectedType, setSelectedType] = useState<TransactionType>("expense");
 
   const handleChange = (
@@ -30,40 +31,70 @@ const CategoryChart = ({ monthlyTransactions, isLoading }:CategoryChartProps ) =
     return acc;
   }, {} as Record<IncomeCategory | ExpenseCategory, number>);
 
-  console.log("カテゴリーさむ", categorySums)
-  const categoryLabels = Object.keys(categorySums)
-  const categoryValues = Object.values(categorySums)
+  // console.log("カテゴリーさむ", categorySums)
+  const categoryLabels = Object.keys(categorySums) as (IncomeCategory | ExpenseCategory)[];
+  const categoryValues = Object.values(categorySums);
 
   // const w = [...categoryLabels, ...categoryValues];
-  console.log("カテゴリーラベル！", categoryLabels)
-  console.log("カテゴリーばりゅー！", categoryValues)
+  // console.log("カテゴリーラベル！", categoryLabels)
+  // console.log("カテゴリーばりゅー！", categoryValues)
 
   const options = {
     maintainAspectRatio: false,
     responsive: true,
   };
 
+  const incomeCategoryColor:Record<IncomeCategory, string> = {
+    給与 : theme.palette.incomeCategoryColor.給与,
+    副収入: theme.palette.incomeCategoryColor.副収入,
+    お小遣い: theme.palette.incomeCategoryColor.お小遣い
+  }
+
+  const expenseCategoryColor:Record<ExpenseCategory, string> = {
+    食費: theme.palette.expenseCategoryColor.食費,
+    日用品: theme.palette.expenseCategoryColor.日用品,
+    居住費: theme.palette.expenseCategoryColor.居住費,
+    交際費: theme.palette.expenseCategoryColor.交際費,
+    娯楽: theme.palette.expenseCategoryColor.娯楽,
+    交通費: theme.palette.expenseCategoryColor.交通費
+  };
+  const getCategoryColor = (
+    category: IncomeCategory | ExpenseCategory
+  ):string => {
+    if(selectedType === "income") {
+      return incomeCategoryColor[category as IncomeCategory];
+    } else {
+      return expenseCategoryColor[category as ExpenseCategory];
+    }
+  }
+
   const data: ChartData<"pie"> = {
     labels: categoryLabels,
     datasets: [
       {
         data: categoryValues,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
+        // backgroundColor: [
+        //   'rgba(255, 99, 132, 0.2)',
+        //   'rgba(54, 162, 235, 0.2)',
+        //   'rgba(255, 206, 86, 0.2)',
+        //   'rgba(75, 192, 192, 0.2)',
+        //   'rgba(153, 102, 255, 0.2)',
+        //   'rgba(255, 159, 64, 0.2)',
+        // ],
+        backgroundColor: categoryLabels.map((category) =>
+          getCategoryColor(category)
+        ),
+        borderColor: categoryLabels.map((category) =>
+          getCategoryColor(category)
+        ),
+        // borderColor: [
+        //   'rgba(255, 99, 132, 1)',
+        //   'rgba(54, 162, 235, 1)',
+        //   'rgba(255, 206, 86, 1)',
+        //   'rgba(75, 192, 192, 1)',
+        //   'rgba(153, 102, 255, 1)',
+        //   'rgba(255, 159, 64, 1)',
+        // ],
         borderWidth: 1,
       },
     ],
